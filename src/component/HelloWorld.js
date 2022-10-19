e8c182a4c62359763d40e402222c04f5e88fdfbd
AjayPoland <ajaymanger21@gmail.com>
2022-10-20 00:23:29 +0200

    minor changes

--- src/component/HelloWorld.js
+++ src/component/HelloWorld.js
@@ -3,7 +3,8 @@ import React from 'react'
 const HelloWorld = () => {
   return (
     <div>
-      <h1>HelloWorld</h1>
+        <h1>Functional Component</h1>
+      <p>HelloWorld</p>
     </div>
   )
 }
