9fadb51fff419b2c003da1d356b84a741dfb36e0
AjayPoland <ajaymanger21@gmail.com>
2022-10-20 11:53:21 +0200

    adding classComponent HelloAjay.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import HelloAjay from './component/HelloAjay';
 import HelloWorld from './component/HelloWorld';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld/>
+      <HelloAjay/>
     </div>
   );
 }
---
+++ src/component/HelloAjay.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HelloAjay extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Ajay</h1>
+      </div>
+    )
+  }
+}
