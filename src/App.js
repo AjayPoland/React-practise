abafb2531db742a7c4478f8347b138004b9c8a15
AjayPoland <ajaymanger21@gmail.com>
2022-10-23 20:19:29 +0200

    added getter and setter method to the object

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import MethodConstructor from './component/MethodConstructor';
 import Object from './component/Object';
 // import HelloAjay from './component/HelloAjay';
-// import HelloWorld from './component/HelloWorld';
+import HelloWorld from './component/HelloWorld';
 // import Login from './component/Login';
 
 function App() {
   return (
     <div className="App">
-      {/* <HelloWorld creater="Ajay" country="INDIA"/>
+      <HelloWorld creater="Ajay" country="INDIA"/>
       {/* <HelloWorld creater="Ajay" country="POLAND"/> */}
       {/* <HelloAjay name="Priyanka" position="Assistant"/> */}
       {/* <HelloAjay name="Bijoy" position="Casher" /> */}
--- src/component/HelloWorld.js
+++ src/component/HelloWorld.js
@@ -3,11 +3,30 @@ import React from "react";
 const HelloWorld = (props) => {
   //destructing props.
   const { creater, country } = props;
+  const obj = {
+    side: '',
+    set setSide(value){
+      this.side=value;
+    },
+    get getArea(){
+     return Math.pow(this.side,2)
+    }
+  };
+  //self-envoking function.
+  // (function(){
+  //   obj.setSide=16;
+  // })();
+  obj.setSide=9;
+  console.log(obj.getArea);
+  const area=obj.getArea;
   return (
     <div>
       <h1>Functional Component</h1>
       <p>
         HelloWorld, Representing: {country} <br /> Creater: {creater}
+        <br/>
+        side: {obj.side} unit <br/>
+        Area of the square is: {area} sqUnit.
       </p>
     </div>
   );
