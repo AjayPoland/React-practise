cd8b5e2e94a52d8f3ecf2dec57d10e6068af26c4
AjayPoland <ajaymanger21@gmail.com>
2022-10-23 19:53:31 +0200

    add call,apply and bind method

--- src/component/Object.js
+++ src/component/Object.js
@@ -15,17 +15,43 @@ export default function Object() {
         city
       );
     },
+    personBIO: function(age,gen){
+      return(
+      `First Name: ${this.FristName }
+      Last Name: ${this.LastName}
+      Age: ${age}
+      Gender:  ${gen}`
+      )
+    }
   };
+  const objBind={
+    num1: 14,
+    num2: 12,
+    add: function(){
+      return this.num1+this.num2;
+    }
+  }
+  const numbers={
+    num1: 15,
+    num2: 15
+  }
   const person = {
     FristName: "Ajay",
     LastName: "Manger",
   };
   const str = object.display.call(person);
   const str1 = object.fullAddress.call(person, "Siliguri");
+  const str2 = object.personBIO.apply(person,[26,"Male"]);
+  // const str3 = objBind.add.bind(numbers);
+  const str3 = objBind.add.bind(numbers);
   return <div>
     <p>Using call method</p>
     {str}
     <br/>
     {str1}
+    <br/>
+    {str2}
+    <br/>
+    {str3()}
   </div>;
 }
