a267ecfb58b2d430b558f79e30762b14bca72d1c
AjayPoland <ajaymanger21@gmail.com>
2022-10-29 09:46:21 +0200

    added values() and entries() in SetDemo.js

--- src/component/SetDemo.js
+++ src/component/SetDemo.js
@@ -11,9 +11,39 @@ export default function SetDemo() {
     demoSet.forEach((value,index,arr) => {
         setValue+=index +"\n\n";
     });
+    //Using different methods. with sets
+    const st_vl=demoSet.values();
+    console.log("demoSet.values()" +st_vl)
+    let st_vl_rslt= "";
+    for(let x of st_vl){
+      st_vl_rslt +=x +"\n";
+    }
+    console.log("demoSet.entries()"+demoSet.entries());
+    let st_vl_entries="";
+    let st_ky_entries="";
+    for(const [keys,values] of demoSet.entries()){
+      //here keys and values has same output, coz set are itself unique, it has no key value.
+      st_vl_entries+=values+"\n";
+      st_ky_entries+=keys +"\n";
+    }
   return (
     <div>
+      <h2>forEach() output of sets:</h2>
       {setValue}
+      <br/>
+      <h2>values() output of sets:</h2>
+      {st_vl_rslt}
+      <br/>
+      <h2>entries() output of sets value:</h2>
+      <p><i>entries() return the iterable object with [keys,values] pairs.</i></p>
+      {st_vl_entries}
+      <br/>
+      <h2>entries() output of sets keys:</h2>
+      <p><i>entries() return the iterable object with [keys,values] pairs.</i></p>
+      <p></p>
+      {st_ky_entries}
+      <br/>
+      <b><strong><i>Notice: </i></strong> check the console log.</b>
     </div>
   )
 }
